5fccc02b9034ef042f2e840986204bb4367631b9
Ahmet Uzundağ <ahmetrabet@gmail.com>
2026-03-11 12:31:43 +0300

    Update swapop.c and test.c; add new push_swap executable
    - Updated the header comment in swapop.c to reflect the latest modification date.
    - Refactored test.c to remove unnecessary struct and commented-out code, simplifying the main function.
    - Introduced a new push_swap executable with initial binary data.

--- push_swap.c
+++ push_swap.c
@@ -6,82 +6,54 @@
 /*   By: auzundag <auzundag@student.42istanbul.com.tr  + +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/10 08:59:16 by auzundag          #+#    #+#             */
-/*   Updated: 2026/03/10 15:26:11 by auzundag         ###   ########.fr       */
+/*   Updated: 2026/03/11 12:30:12 by auzundag         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 #include <stdio.h>
 
-// static int	is_number(char *argv)
+// static t_stack	*newNode(int data)
 // {
-// 	// if sign varsa geç
-// 	// geri kalan her şey digit olmalı
+// 	t_stack	*node;
+
+// 	node = malloc(sizeof(t_stack));
+// 	if (!node)
+// 		return (NULL);
+// 	node->data = data;
+// 	node->next = NULL;
+// 	return (node);
 // }
 
-// static t_stack	*parse_input(int argc, char **argv)
-// {
-// }
-
-static t_stack	*newNode(int data)
+static int	isNumber(char *str)
 {
-	t_stack	*node;
+}
 
-	node = malloc(sizeof(t_stack));
-	if (!node)
-		return (NULL);
-	node->data = data;
-	node->next = NULL;
-	return (node);
+static int	parseNumber(char *token)
+{
 }
 
 int	main(int argc, char **argv)
 {
-	t_stack	*a_stack;
-	t_stack	*b;
 	int		i;
-	char	**tokens;
 	int		j;
-	t_stack	*node1;
-	t_stack	*node2;
-	t_stack	*node3;
-	t_stack	*temp;
+	char	**tokens;
 
 	if (!argv)
 		write(1, "error\n", 6);
 	if (argc < 2)
 		return (0);
 	i = 1;
-	j = 0;
 	while (i < argc)
 	{
-		tokens = ft_split(argv[i], ' ');
+		tokens = ft_split(argv[i++], ' ');
 		j = 0;
 		while (tokens[j])
 		{
-			// printf("%s\n", tokens[j]);
+			parseNumber(tokens[j]);
 			j++;
 		}
 		i++;
-	}
-	node1 = newNode(1);
-	node2 = newNode(2);
-	node3 = newNode(3);
-	node1->next = node2;
-	node2->next = node3;
-	printf("Before\n");
-	temp = node1;
-	while (temp)
-	{
-		printf("%d\n", temp->data);
-		temp = temp->next;
-	}
-	rra(&node1);
-	printf("After\n");
-	while (node1)
-	{
-		printf("%d\n", node1->data);
-		node1 = node1->next;
 	}
 }
 
@@ -91,20 +63,6 @@ int	main(int argc, char **argv)
 // argv[1] = "3"
 // argv[2] = "2"
 // argv[3] = "1"
-
-// argv
-//  ↓
-// for each argv[i]
-//  ↓
-// split
-//  ↓
-// numbers list
-//  ↓
-// validate
-//  ↓
-// atoi
-//  ↓
-// stack
 
 // edge caseler
 // 4a2
@@ -116,8 +74,20 @@ int	main(int argc, char **argv)
 // push_swap programı. Programınız çalışma zamanında bir strateji seçebilmelidir
 // Giriş yapılandırması üzerinde
 
-// argv
-// ↓
+// main
+//  ↓
+// parse_args
+//  ↓
+// argv[i]
+//  ↓
 // split
-// ↓
-// tokens[]
+//  ↓
+// tokens[j]
+//  ↓
+// validate_number
+//  ↓
+// ft_atol
+//  ↓
+// check_duplicate
+//  ↓
+// stack_add_back
--- test.c
+++ test.c
@@ -1,26 +1,4 @@
-typedef struct s_node
+int main()
 {
-	int 			data;
-	struct s_node 	*next;
-} t_node;
 
-
-// int stack[100];
-// int top = -1;
-
-// void push(int x) {
-//     stack[++top] = x;
-// }
-
-// int pop() {
-//     return (stack[top--]);
-// }
-
-#include <stdio.h>
-#include <stdlib.h>
-
-int main(void)
-{
-	t_node *top = NULL;
-	top->data = 42;
 }
