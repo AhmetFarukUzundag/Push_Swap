ecb017aaac3c6874a1623c25a324f25ef4f3c9ef
AhmetFarukUzundag <ahmetrabet@gmail.com>
2026-03-22 16:01:05 +0300

    feat: implement ft_strncmp function for string comparison

---
+++ src/ft_strncmp.c
@@ -0,0 +1,17 @@
+#include "push_swap.h"
+
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	int	i;
+
+	i = 0;
+	while (n > 0 && (s1[i] || s2[i]))
+	{
+		if (s1[i] != s2[i])
+			return (((unsigned char *)s1)[i] - ((unsigned char *)s2)[i]);
+		i++;
+		n--;
+	}
+	return (0);
+}
+	
